6c001cd6cfc99bdd8fd574dc9d7d59b355f8c4bf
devgasparotto <devgasp@gmail.com>
2017-11-17 20:25:25 -0500

    test chmod ruby

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
 		require 'pdf_forms'
 
 		FileUtils.chmod 0777, '/app/bin/pdftk/bin', :verbose => true
-
+		puts File.stat("/app/bin/pdftk/bin").mode.to_s(8)[3..5]
 		# pdftk = PdfForms.new('/app/bin/pdftk/bin')
 		# pdftk.get_field_names '/app/documents/DisclosureRequestLetter.pdf'
 		#pdftk = PdfForms.new('pdftk')
