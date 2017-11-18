860e5fa79b4068b7fa56a3193445c48e6e5577b2
devgasparotto <devgasp@gmail.com>
2017-11-17 19:14:15 -0500

    pdftk test

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
 
 	def testPDF
-		puts "/app/vendor/pdftk/bin/pdftk: " File.exist?("/app/vendor/pdftk/bin/pdftk")
-		puts "/vendor/pdftk/bin/pdftk: " File.exist?("/vendor/pdftk/bin/pdftk")
+		puts "/app/vendor/pdftk/bin/pdftk: " + File.exist?("/app/vendor/pdftk/bin/pdftk")
+		puts "/vendor/pdftk/bin/pdftk: " + File.exist?("/vendor/pdftk/bin/pdftk")
 		#pdftk = PdfForms.new('pdftk')
 
 	end
