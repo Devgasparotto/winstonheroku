17f5b59a9294596ae72cd37ad7a13dfd0462ffae
devgasparotto <devgasp@gmail.com>
2017-11-17 19:48:20 -0500

    test pdftk

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,8 @@ class WelcomeController < ApplicationController
 		fileExist = File.exist?("/app/bin/pdftk/bin/pdftk")
 		puts "/app/bin/pdftk/bin/pdftk: #{fileExist}"
 		puts File.stat("/app/bin/pdftk/bin/pdftk").mode.to_s(8)[3..5]
-		pdftk = PdfForms.new('/app/vendor/pdftk/bin/pdftk')
+		#pdftk = PdfForms.new('/app/bin/pdftk/bin/pdftk')
+		pdftk = PdfForms.new('pdftk')
 		render html: "HI"
 	end
 
