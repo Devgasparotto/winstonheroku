e3d11fe18d4c301ca16025f551480c30c126ce41
devgasparotto <devgasp@gmail.com>
2017-11-17 20:07:37 -0500

    test pdftk

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,7 @@ class WelcomeController < ApplicationController
 	def testPDF
 		require 'pdf_forms'
 
-		fileExist = File.exist?("/app/documents/DisclosureRequestLetter.pdf")
-		puts "/app/documents/DisclosureRequestLetter.pdf: #{fileExist}"
-		#puts File.stat("/app/bin/pdftk/bin/pdftk").mode.to_s(8)[3..5]
-		pdftk = PdfForms.new('pdftk')
+		pdftk = PdfForms.new('/app/bin/pdftk/bin')
 		pdftk.get_field_names '/app/documents/DisclosureRequestLetter.pdf'
 		#pdftk = PdfForms.new('pdftk')
 		render html: "HI"
