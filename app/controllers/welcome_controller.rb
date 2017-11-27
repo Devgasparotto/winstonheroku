5c136f057a3bbb240aa770b77b48f5dca7d76336
devgasparotto <devgasp@gmail.com>
2017-11-27 16:57:36 -0500

    PDFTK Debugging

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,8 +8,15 @@ class WelcomeController < ApplicationController
 		# FileUtils.chmod 0777, pdftkFilePath, :verbose => true
 		# puts File.stat(pdftkFilePath).mode.to_s(8)
 		pdftk = PdfForms.new(pdftkFilePath)
-		puts pdftk.get_field_names '/app/documents/DisclosureRequestLetter.pdf'
-		#pdftk = PdfForms.new('pdftk')
+		#puts pdftk.get_field_names '/app/documents/DisclosureRequestLetter.pdf'
+		templateDisclosureLetterPath = '/app/documents/DisclosureRequestLetter.pdf'
+		disclosureLetterPath = '/app/documents/test.pdf'
+
+		pdftk.fill_form templateDisclosureLetterPath, disclosureLetterPath, {
+			:courtAddressLine1 => "courtAddressLine1", :courtAddressLine2 => "courtAddressLine2",
+			:flatten => true
+
+
 		render html: "HI"
 	end
 
