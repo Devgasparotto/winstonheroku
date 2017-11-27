e7067ab0475b95920574cd70f692a26c210c3afe
devgasparotto <devgasp@gmail.com>
2017-11-27 17:08:39 -0500

    DebuDebugging PDFTK

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
 		require 'pdf_forms'
 		pdftkFilePath = '/app/bin/pdftk/bin/pdftk' #For Heroku Prod
 		#pdftkFilePath = #'/bin/pdftk/bin/pdftk'
-		puts File.exist?(pdftkFilePath)
+		#puts File.exist?(pdftkFilePath)
 		# FileUtils.chmod 0777, pdftkFilePath, :verbose => true
 		# puts File.stat(pdftkFilePath).mode.to_s(8)
 		pdftk = PdfForms.new(pdftkFilePath)
@@ -12,11 +12,17 @@ class WelcomeController < ApplicationController
 		templateDisclosureLetterPath = '/app/documents/DisclosureRequestLetter.pdf'
 		disclosureLetterPath = '/app/documents/test.pdf'
 
+		if File.exist?(disclosureLetterPath)
+			puts "File exists before deletion"
+			File.delete(disclosureLetterPath)
+			puts "deleted file"
+		end
+
 		pdftk.fill_form templateDisclosureLetterPath, disclosureLetterPath, {
 			:courtAddressLine1 => "courtAddressLine1", :courtAddressLine2 => "courtAddressLine2"},
 			:flatten => true
 
-
+		puts "Created " + File.exist?(disclosureLetterPath)
 		render html: "HI"
 	end
 
