d919d21b58ea14c52f421f2a3429a32a253a4519
devgasparotto <devgasp@gmail.com>
2017-12-01 14:59:56 -0500

    test email

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -23,6 +23,7 @@ class WelcomeController < ApplicationController
 			:flatten => true
 
 		puts File.exist?(disclosureLetterPath)
+		testRoute(disclosureLetterPath)
 		render html: "HI"
 	end
 
@@ -33,48 +34,11 @@ class WelcomeController < ApplicationController
 		end
 	end
 
-	def testEmail
-		require 'rest-client'
-
-		apikey = 'key-8ff2b34368c52e42b2e202035ddc7e6c'
-		#API_KEY = ENV['MAILGUN_API_KEY']
-		apiurl = "https://api:#{apikey}@api.mailgun.net/v2/sandbox2496379cc7f445659f2aba340feb5986.mailgun.org"
- 
-		RestClient.post apiurl+"/messages",
-		    :from => "devgasp@gmail.com",
-		    :to => "devgasp@gmail.com",
-		    :subject => "This is subject",
-		    :text => "Text body",
-		    :html => "<b>HTML</b> version of the body!"
+	def testEmail(pdfFilePath)
+		respond_to do |format|
+			FormMailer.test_email(pdfFilePath).deliver_now
+			format.html {render html: "Success"}
+		end
 	end
-
-	# def FormatPDFEmail(pdfFilePath)
-	# 	subject = "PDF Email Delivery"
-	# 	html = "<html><p>PDF Form Delivery</p>" +
-	# 	"<p>File Path: " + pdfFilePath + "</p>" +
-	# 	"</html>"
-	# 	# respond_to do |format|
-	# 	# 	EmailPDF(pdfFilePath).deliver_now
-	# 	# 	format.html {render html: "Success"}
-	# 	# end
-	# 	EmailPDF(pdfFilePath, subject, html)
-	# end
-
-	# def EmailPDF(pdfFilePath, subject, html)
- #    	pdfFile = File.open(pdfFilePath, "r")
-    
- #    	mg_client = Mailgun::Client.new 'key-8ff2b34368c52e42b2e202035ddc7e6c'
- #    	message_params = {:from    => 'devgasp@gmail.com',
- #                      :to      => 'devgasp@gmail.com',
- #                      :subject => subject,
- #                      :text    => "File sent.",
- #                      :html    => html,
- #                      :attachment => pdfFile}
- #    	mg_client.send_message 'sandbox2496379cc7f445659f2aba340feb5986.mailgun.org', message_params
-      
- #    	if File.exist?(pdfFilePath)
- #    		#File.delete(pdfFilePath)
- #    	end
-	# end
 
 end
--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -2,6 +2,6 @@ class FormMailer < ApplicationMailer
 	default from: 'devgasp@gmail.com'
 
 	def test_email(pdfFilePath)
-		mail(to: 'devgasp@gmail.com', subject: 'Test Email')
+		mail(to: 'devgasp@gmail.com', subject: "Test Email #{pdfFilePath}")
 	end
 end
