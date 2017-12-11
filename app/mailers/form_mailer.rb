860e0754f3bf6d8b43600201572dc63feedeee44
devgasparotto <devgasp@gmail.com>
2017-12-11 12:03:32 -0500

    Form mailer template

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -2,7 +2,7 @@ class FormMailer < ApplicationMailer
 	default from: 'devgasp@gmail.com'
 
 	def sample_pdf_email(pdfFilePath)
-		attachments['test.pdf'] = File.read(pdfFilePath)
+		#attachments['test.pdf'] = File.read(pdfFilePath)
 		mail(to: 'devgasp@gmail.com', subject: "Test Email #{pdfFilePath}")
 	end
 end
