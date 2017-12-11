f945d63aca291b473343f394d13ba7bf72f43763
devgasparotto <devgasp@gmail.com>
2017-12-11 13:08:11 -0500

    Form mailer template

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -1,8 +1,8 @@
 class FormMailer < ApplicationMailer
-	default from: 'tsbotmanager@gmail.com' #'devgasp@gmail.com'
+	default from: 'tsbotmanager@gmail.com'
 
 	def sample_pdf_email(pdfFilePath)
-		#attachments['test.pdf'] = File.read(pdfFilePath)
+		attachments['test.pdf'] = File.read(pdfFilePath)
 		mail(to: 'tsbotmanager@gmail.com', subject: "Test Email #{pdfFilePath}")
 	end
 end
