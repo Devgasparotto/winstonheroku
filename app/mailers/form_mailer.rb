4bfba14133ce987cf3bebcf9c03d63317f436ea8
devgasparotto <devgasp@gmail.com>
2017-12-11 12:21:33 -0500

    Form mailer template

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -1,5 +1,5 @@
 class FormMailer < ApplicationMailer
-	default from: 'devgasp@gmail.com'
+	default from: 'mailgun@sandbox2496379cc7f445659f2aba340feb5986.mailgun.org' #'devgasp@gmail.com'
 
 	def sample_pdf_email(pdfFilePath)
 		#attachments['test.pdf'] = File.read(pdfFilePath)
