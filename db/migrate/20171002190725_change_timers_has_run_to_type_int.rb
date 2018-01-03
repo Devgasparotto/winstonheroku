3b07557649ddd5aede4e6a0c2946d0ad28472932
devgasparotto <devgasp@gmail.com>
2018-01-03 18:04:42 -0500

    Fixing migrations

--- db/migrate/20171002190725_change_timers_has_run_to_type_int.rb
+++ db/migrate/20171002190725_change_timers_has_run_to_type_int.rb
@@ -1,6 +1,6 @@
 class ChangeTimersHasRunToTypeInt < ActiveRecord::Migration[5.0]
   def up
-    change_column :timers, :HasRun, 'integer USING CAST(column_name AS integer)'
+    change_column :timers, :HasRun, 'integer USING CAST(HasRun AS integer)'
   end
 
   def down
