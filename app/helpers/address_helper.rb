4c6b0c4eaa24b6fde6031682458cd5908ae12a1a
devgasparotto <devgasp@gmail.com>
2018-01-03 22:11:46 -0500

    Fixed address duplication issue when saving NOI form

--- app/helpers/address_helper.rb
+++ app/helpers/address_helper.rb
@@ -1,9 +1,8 @@
 module AddressHelper
 	def SetDefendantMailingAddressByUserId(ticketId, streetNo, street, apt, municipality, province, postalCode)
 		ticketProperties = TicketProperty.find_by(:TicketId => ticketId)
-		address = Address.find_by(:StreetNo => streetNo, :Street => street, :Apt => apt, :StreetNo => streetNo, :Municipality => municipality, :Province => province, :PostalCode => postalCode)
 
-		if !address.present?
+		if !ticketProperties[:DefendantMailingAddressId].present?
 			address = Address.new
 			address.StreetNo = streetNo
 			address.Street = street
@@ -11,23 +10,20 @@ module AddressHelper
 			address.Municipality = municipality
 			address.Province = province
 			address.PostalCode = postalCode
-			puts address
 			address.save
-		end
 
-		# if !ticketProperties[:DefendantMailingAddressId].present?
-		ticketProperties.DefendantMailingAddressId = address[:id]
-		ticketProperties.save
-		# else
-		# 	address = Address.find_by(id: ticketProperties[:DefendantMailingAddressId])
-		# 	address.StreetNo = streetNo
-		# 	address.Street = street
-		# 	address.Apt = apt
-		# 	address.Municipality = municipality
-		# 	address.Province = province
-		# 	address.PostalCode = postalCode
-		# 	address.save
-		# end		
+			ticketProperties.DefendantMailingAddressId = address[:id]
+			ticketProperties.save
+		else
+			address = Address.find_by(id: ticketProperties[:DefendantMailingAddressId])
+			address.StreetNo = streetNo
+			address.Street = street
+			address.Apt = apt
+			address.Municipality = municipality
+			address.Province = province
+			address.PostalCode = postalCode
+			address.save
+		end		
 	end
 
 	def GetMailingAddressByTicketId(ticketId)
