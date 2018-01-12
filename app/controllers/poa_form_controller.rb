423a8db082111dbb243103abbe9145e0fcd43b11
devgasparotto <devgasp@gmail.com>
2018-01-12 13:26:50 -0500

    Disclosure form

--- app/controllers/poa_form_controller.rb
+++ app/controllers/poa_form_controller.rb
@@ -31,7 +31,8 @@ class PoaFormController < ApplicationController
 		correctedURL = URI.unescape(request.original_url)
 		uri    = URI.parse(correctedURL)
 		params = CGI.parse(uri.query) #NOTE: this allows the "%3D" to be interpretered as "=" in the query string params when displaying the webpage
-
+		puts params[:id]
+		puts params['id']
 		if params['id'].nil? || params['id'].empty?
 			render html: "The user could not be verified"
 		else
