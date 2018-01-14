104f510cf9128df4d113a3becda95c56f2feb11a
devgasparotto <devgasp@gmail.com>
2018-01-14 11:50:14 -0500

    User Registration

--- app/controllers/chatfuel_registration_controller.rb
+++ app/controllers/chatfuel_registration_controller.rb
@@ -2,10 +2,9 @@ class ChatfuelRegistrationController < ApplicationController
 	
 	def RegisterChatfuelUser		
 		render html: "RegisterChatfuelUser"
-
-		if !UserFBIdExists(params['chatfuel user id'])
-			parsedProfilePictureURL = helpers.ParseProfilePicURL(params['profile pic url'])
-			user = User.new(FacebookId: params['chatfuel user id'], ProfilePicURL: parsedProfilePictureURL, SenderId: params['messenger user id'])
+		id = params['messenger user id']
+		if !UserFBIdExists(id)
+			user = User.new(FacebookId: id, SenderId: id)
 			user.save
 		end
 	end
